cb1b2fe3939ad92528a33bd53b289c24ea178112
orlando mauricio jimenez cedeño <Orlando_Jmz86@hotmail.com>
2018-09-30 10:26:46 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+
+class Escuela
+{
+   public string nombre;
+   public string direccion;
+   public int añofundacion;
+   public string ceo ;
+
+    public void Timbrar()
+    {
+        // to do
+        Console.Beep(2000,3000);
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+
+           var miEscuela = new Escuela();
+           miEscuela.nombre = "Platzi Academy";
+           miEscuela.direccion = "Calle 9 calle 72";
+           miEscuela.añofundacion = 2012;
+           Console.WriteLine("Timbrando");
+           miEscuela.Timbrar();
+           
+        }
+    }
+}
